eebdc7de90a7f0a6e1f19c741ce61b7f525f1174
Koen Van Geert <koen.vangeert@collibra.com>
2026-07-23 10:00:25 +0200

    feat(jira): add Cmd+I shortcut to jump to the Jira view

--- plugins/jira/src/index.ts
+++ plugins/jira/src/index.ts
@@ -15,6 +15,7 @@ export default defineFrontendPlugin({
         icon: 'file-text',
         placement: 'rail',
         order: 40,
+        shortcut: 'Cmd+I',
         component: JiraQueryView,
       }),
     )
